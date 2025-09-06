539c3f673577e702a604f99cd3bc135805c31750
Chris <chris@grislygrotto.nz>
2025-09-06 14:21:03 +1200

    re-enabled scaling

--- scripts/MainScene.cs
+++ scripts/MainScene.cs
@@ -5,7 +5,7 @@ public partial class MainScene : Node3D
 {
     [Export] public PackedScene CardScene { get; set; }
 
-    private readonly Vector3 target_card_size = new(1, 0, 1.5f);
+    private readonly Vector2 target_card_size = new(1, 1.5f);
 
     private readonly List<Card> cards = [];
 
@@ -22,8 +22,8 @@ public partial class MainScene : Node3D
                 AddChild(new_card);
 
                 var native_size = new_card.GetSize();
-                //new_card.Scale = new(target_card_size.X / native_size.X, target_card_size.Y, target_card_size.Z / native_size.Z);
-                new_card.GlobalPosition = new Vector3(i * (target_card_size.X + gap.X), 1f, j * (target_card_size.Z + gap.Z));
+                new_card.Scale = new(target_card_size.X / native_size.X, target_card_size.Y, target_card_size.Y / native_size.Y);
+                new_card.GlobalPosition = new Vector3(i * (target_card_size.X + gap.X), 1f, j * (target_card_size.Y + gap.Y));
 
                 cards.Add(new_card);
                 new_card.SetDepth(cards.Count * depth_adjust);
