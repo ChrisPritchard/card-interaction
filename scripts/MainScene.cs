ccdb50745d8def3453e7206454ed74143de326b9
Chris <chris@grislygrotto.nz>
2025-09-06 14:18:16 +1200

    fixed transparency with the depth approach (using scissor cutoff)

--- scripts/MainScene.cs
+++ scripts/MainScene.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class MainScene : Node3D
 {
     [Export] public PackedScene CardScene { get; set; }
-    [Export] public Camera3D Camera;
 
-    private readonly Vector3 target_card_size = new(1, 0.1f, 1.5f);
+    private readonly Vector3 target_card_size = new(1, 0, 1.5f);
+
+    private readonly List<Card> cards = [];
+
+    private const float depth_adjust = 0.01f;
 
     public override void _Ready()
     {
@@ -16,9 +20,13 @@ public partial class MainScene : Node3D
             {
                 var new_card = CardScene.Instantiate<Card>();
                 AddChild(new_card);
+
                 var native_size = new_card.GetSize();
-                new_card.Scale = new(target_card_size.X / native_size.X, target_card_size.Y, target_card_size.Z / native_size.Y);
+                //new_card.Scale = new(target_card_size.X / native_size.X, target_card_size.Y, target_card_size.Z / native_size.Z);
                 new_card.GlobalPosition = new Vector3(i * (target_card_size.X + gap.X), 1f, j * (target_card_size.Z + gap.Z));
+
+                cards.Add(new_card);
+                new_card.SetDepth(cards.Count * depth_adjust);
             }
     }
 
