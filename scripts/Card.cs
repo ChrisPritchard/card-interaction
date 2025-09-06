083dfbee0934df4e4bdf5c65062f8c21752cae1a
Chris <chris@grislygrotto.nz>
2025-09-06 14:18:16 +1200

    fixed transparency with the depth approach (using scissor cutoff)

--- scripts/Card.cs
+++ scripts/Card.cs
@@ -1,11 +1,26 @@
 
+using System.Threading.Tasks;
 using Godot;
 
-public partial class Card : Sprite3D
+public partial class Card : MeshInstance3D
 {
     [Export] public Area3D CollisionArea;
 
+
     internal void SetCollisionLayer(int layer) => CollisionArea.CollisionLayer = (uint)(0 << (layer - 1));
 
-    internal Vector2 GetSize() => Texture.GetSize() * PixelSize;
+    internal Vector3 GetSize() => GetAabb().Size;
+
+    public override void _Ready()
+    {
+        SetSurfaceOverrideMaterial(0, (Material)GetActiveMaterial(0).Duplicate());
+    }
+
+    internal void SetDepth(float value)
+    {
+        if (!IsNodeReady())
+            CallDeferred(nameof(SetDepth), value);
+        else
+            (GetSurfaceOverrideMaterial(0) as ShaderMaterial).SetShaderParameter("depth_setting", value);
+    }
 }
